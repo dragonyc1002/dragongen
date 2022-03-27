9979a611dae0919ebeea2612c0f9d5a0d111bc51
hugocoding <mivck1012@gmail.com>
2022-03-27 17:28:09 +0800

    update to v0.0.1

--- src/renderer.js
+++ src/renderer.js
@@ -2,8 +2,8 @@ import React from 'react';
 import avatar_dragon from './asset/avatar-dragon.png';
 import avatar_emu800jpn from './asset/avatar-emu800jpn.png';
 import avatar_emu900 from './asset/avatar-emu900.png';
-import avatar_FHfinal from './asset/FHfinal';
-import avatar_emu1200final from './asset/EMU1200final';
+import avatar_FHfinal from './asset/FHfinal.png';
+import avatar_emu1200final from './asset/EMU1200final.png';
 import {
   DiscordMessages,
   DiscordMessage,
